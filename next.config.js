a192fd18a9eb03cc70f83e351ec0414ca3964497
Griffin Davis <griffin04901@gmail.com>
2026-05-03 17:09:48 -0500

    fix action

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === "production";
-const repoName = "My-Website";
-
 const nextConfig = {
-  output: "export",
-  basePath: isProduction ? `/${repoName}` : "",
-  assetPrefix: isProduction ? `/${repoName}/` : undefined
+  output: "export"
 };
 
 module.exports = nextConfig;
